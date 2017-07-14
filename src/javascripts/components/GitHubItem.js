e2f51b77946cae1d7b0dc999137d3e15c02ced33
s4kr4 <s4kr4m4@gmail.com>
2017-07-14 13:08:53 +0900

    Add fork icon instead of characters

--- src/javascripts/components/GitHubItem.js
+++ src/javascripts/components/GitHubItem.js
@@ -1,5 +1,6 @@
 //@flow
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
 
 export default class GitHubItem extends Component {
   props: {
@@ -10,13 +11,29 @@ export default class GitHubItem extends Component {
 
   constructor(props) {
     super(props)
+
+    this.forkIcon = this.forkIcon.bind(this)
+  }
+
+  forkIcon() {
+    return (
+      <FontAwesome
+        className="repo-fork-icon"
+        name="code-fork"
+        size="lg"
+        tag="i"
+      />
+    )
   }
 
   render() {
     return (
       <li className="item github-item">
         <a href={this.props.url} target="_blank">{this.props.name}</a>
-        {this.props.fork ? "(fork)" : ""}
+        {this.props.fork
+          ? this.forkIcon()
+          : ""
+        }
       </li>
     )
   }
