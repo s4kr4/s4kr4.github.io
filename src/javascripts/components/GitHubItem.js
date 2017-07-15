6e251492f2f89421bbd1074d7c4f471516f46c96
s4kr4 <s4kr4m4@gmail.com>
2017-07-15 20:28:26 +0900

    Update repository list

--- src/javascripts/components/GitHubItem.js
+++ src/javascripts/components/GitHubItem.js
@@ -13,12 +13,24 @@ export default class GitHubItem extends Component {
     super(props)
 
     this.forkIcon = this.forkIcon.bind(this)
+    this.repoIcon = this.repoIcon.bind(this)
+  }
+
+  repoIcon() {
+    return (
+      <FontAwesome
+        className="repo-icon"
+        name="book"
+        size="lg"
+        tag="i"
+      />
+    )
   }
 
   forkIcon() {
     return (
       <FontAwesome
-        className="repo-fork-icon"
+        className="repo-icon"
         name="code-fork"
         size="lg"
         tag="i"
@@ -29,11 +41,11 @@ export default class GitHubItem extends Component {
   render() {
     return (
       <li className="item github-item">
-        <a href={this.props.url} target="_blank">{this.props.name}</a>
         {this.props.fork
           ? this.forkIcon()
-          : ""
+          : this.repoIcon()
         }
+        <a href={this.props.url} target="_blank">{this.props.name}</a>
       </li>
     )
   }
