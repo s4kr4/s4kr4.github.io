87a383c0e9186a7069895e20eb91da6cea12582d
s4kr4 <s4kr4m4@gmail.com>
2017-11-23 14:31:17 +0000

    Add to homescreen

--- fuse.js
+++ fuse.js
@@ -46,7 +46,7 @@ Sparky.task('clean', () => Sparky.src('public/').clean('public/'))
 Sparky.task('copy', () => Sparky.src('index.html', {base: 'public'}).dest('.'))
 Sparky.task('prod-env', () => { isProduction = true })
 Sparky.task('build', ['prod-env', 'clean', 'config'], () => fuse.run())
-Sparky.task('release', ['build', 'copy'], () => Sparky.src('service-worker.js', {base: 'src'}).dest('public'))
+Sparky.task('release', ['build', 'copy'], () => Sparky.src('service-worker.js', {base: 'src'}).dest('.'))
 
 Sparky.task('default', ['clean', 'config'], () => {
   fuse.dev({
