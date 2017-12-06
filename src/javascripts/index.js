37610ee545efc4f4c619a43565110103f5503831
s4kr4 <s4kr4m4@gmail.com>
2017-12-06 02:58:36 +0000

    Minify sources

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Container from './components/Container.js'
-import '../stylesheets/index.css'
+import '../stylesheets/index.scss'
 
 ReactDOM.render((
   <div className="main">
