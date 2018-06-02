e536794005ed71be60d029a5166c6b555b370a93
s4kr4 <s4kr4m4@gmail.com>
2018-06-02 12:27:40 +0900

    Show image if it's exist

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -9,15 +9,20 @@ interface IProps {
   image?: string
 }
 
-const Work: React.SFC<IProps> = ({ className, title, url }) => (
+const Work: React.SFC<IProps> = ({ className, title, url, image }) => (
   <a className={className} href={url}>
+    {image ? <img src={image} /> : null}
     {title}
   </a>
 )
 
 export default styled(Work)`
   margin: 10px;
-  width: 200px;
-  height: 200px;
+  width: 120px;
+  height: 120px;
   border: #aaa 1px solid;
+
+  img {
+    max-width: 100%;
+  }
 `
